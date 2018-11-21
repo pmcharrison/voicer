7c67431961f28f45239c811602ef3e925cb4cf89
Peter M C Harrison <pmc.harrison@gmail.com>
2018-11-21 17:52:26 +0000

    Updating dependencies

--- R/chord-cost-funs.R
+++ R/chord-cost-funs.R
@@ -15,44 +15,43 @@ voicer_cost_funs <- function(
   min_pitch_weight = 5
 ) {
   list(
-  roughness = seqopt::cost_fun(context_sensitive = FALSE, f = function(x) {
-    HarmonyDissonance::get_roughness(frequency = x,
-                                     frequency_scale = "midi",
-                                     method = "hutch",
-                                     cache = FALSE)
-  }, memoise = TRUE, weight = roughness_weight),
+    roughness = seqopt::cost_fun(context_sensitive = FALSE, f = function(x) {
+      incon::get_roughness(frequency = x,
+                           frequency_scale = "midi",
+                           method = "hutch")
+    }, memoise = TRUE, weight = roughness_weight),
 
-  parallels = seqopt::cost_fun(context_sensitive = TRUE, f = function(context, x) {
-    context_bass <- min(context)
-    context_treble <- max(context)
-    context_int <- context_treble - context_bass
-    if ((context_int %% 12) %in% c(0, 7)) {
-      next_bass <- min(x)
-      next_treble <- max(x)
-      next_int <- next_treble - next_bass
-      if (next_int == context_int) 1 else 0
-    } else 0
-  }, weight = parallels_weight),
+    parallels = seqopt::cost_fun(context_sensitive = TRUE, f = function(context, x) {
+      context_bass <- min(context)
+      context_treble <- max(context)
+      context_int <- context_treble - context_bass
+      if ((context_int %% 12) %in% c(0, 7)) {
+        next_bass <- min(x)
+        next_treble <- max(x)
+        next_int <- next_treble - next_bass
+        if (next_int == context_int) 1 else 0
+      } else 0
+    }, weight = parallels_weight),
 
-  melody_dist = seqopt::cost_fun(context_sensitive = TRUE, f = function(context, x) {
-    abs(max(x) - max(context))
-  }, weight = melody_dist_weight),
+    melody_dist = seqopt::cost_fun(context_sensitive = TRUE, f = function(context, x) {
+      abs(max(x) - max(context))
+    }, weight = melody_dist_weight),
 
-  min_vl_dist = seqopt::cost_fun(context_sensitive = TRUE, f = function(contexts, x) {
-    as.numeric(minVL::min_vl_dists(contexts, list(x), elt_type = "pitch", norm = vl_dist_norm))
-  }, memoise = TRUE, vectorised = TRUE, weight = 10),
+    min_vl_dist = seqopt::cost_fun(context_sensitive = TRUE, f = function(contexts, x) {
+      as.numeric(minVL::min_vl_dists(contexts, list(x), elt_type = "pitch", norm = vl_dist_norm))
+    }, memoise = TRUE, vectorised = TRUE, weight = 10),
 
-  mean_pitch = seqopt::cost_fun(context_sensitive = FALSE, f = function(x) {
-    abs(mean(x) - mean_pitch)
-  }, weight = mean_pitch_weight),
+    mean_pitch = seqopt::cost_fun(context_sensitive = FALSE, f = function(x) {
+      abs(mean(x) - mean_pitch)
+    }, weight = mean_pitch_weight),
 
-  max_pitch = seqopt::cost_fun(context_sensitive = FALSE, f = function(x) {
-    pmax(0, max(x) - max_pitch_boundary)
-  }, weight = max_pitch_weight),
+    max_pitch = seqopt::cost_fun(context_sensitive = FALSE, f = function(x) {
+      pmax(0, max(x) - max_pitch_boundary)
+    }, weight = max_pitch_weight),
 
-  min_pitch = seqopt::cost_fun(context_sensitive = FALSE, f = function(x) {
-    pmax(0, min_pitch_boundary - min(x))
-  }, weight = min_pitch_weight)
+    min_pitch = seqopt::cost_fun(context_sensitive = FALSE, f = function(x) {
+      pmax(0, min_pitch_boundary - min(x))
+    }, weight = min_pitch_weight)
 
   )
 }
--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 
-voiceR v0.0.0.9000
-==================
+# voicer
 
-Chord sequences can be voiced in different ways, some better than others. This package, `voiceR`, tries to find optimal voicings for chord sequences.
+Chord sequences can be voiced in different ways, some better than
+others. This package, `voicer`, tries to find optimal voicings for chord
+sequences.
 
 Status: **pre-alpha**
 
-Example usage
--------------
+## Example usage
 
-`voice_pc_set_seq` finds the optimal voicing for a series of pitch class sets.
+`voice_pc_set_seq` finds the optimal voicing for a series of pitch class
+sets.
 
 ``` r
-library(voiceR)
+library(voicer)
 pc_sets <- list(c(0, 4, 7), c(0, 5, 9), c(2, 7, 11), c(0, 4, 7))
 voice_pc_set_seq(pc_sets)
 #> [[1]]
@@ -31,26 +32,30 @@ voice_pc_set_seq(pc_sets)
 #> [1] 19.23183
 ```
 
-`revoice_midi_chord_seq` takes a sequence of MIDI chords as input and returns an optimally revoiced sequence, ensuring that:
+`revoice_midi_chord_seq` takes a sequence of MIDI chords as input and
+returns an optimally revoiced sequence, ensuring that:
 
--   the pitch class of the bass note is preserved;
--   any duplicated pitch classes are preserved.
+  - the pitch class of the bass note is preserved;
+  - any duplicated pitch classes are
+preserved.
+
+<!-- end list -->
 
 ``` r
 midi <- list(c(60, 64, 67), c(60, 65, 69), c(62, 67, 611), c(60, 64, 67))
 revoice_midi_chord_seq(midi)
 #> [[1]]
-#> [1] 48 64 67
+#> [1] 48 55 76
 #> 
 #> [[2]]
-#> [1] 48 65 69
+#> [1] 48 57 77
 #> 
 #> [[3]]
-#> [1] 50 59 67
+#> [1] 50 59 79
 #> 
 #> [[4]]
-#> [1] 48 64 67
+#> [1] 48 52 79
 #> 
 #> attr(,"cost")
-#> [1] 27.22257
+#> [1] 25.67653
 ```
