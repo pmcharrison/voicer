6057f95d6ad9f9c4dc3e9ee84b0023b1ca921103
Peter M C Harrison <pmc.harrison@gmail.com>
2018-11-22 17:48:32 +0000

    Bugfix

--- R/voice.R
+++ R/voice.R
@@ -1,10 +1,12 @@
+#' @export
 voice <- function(x, opt = voice_opt()) {
   UseMethod("voice")
 }
 
+#' @export
 voice.vec <- function(x, opt = voice_opt()) {
   if (hutil::is.empty(x)) return(x)
-  type <- type(x)
+  type <- hutil::type(x)
   checkmate::qassert(type, "S1")
   f <- paste0("voice.vec_", type)
   do.call(f, args = list(x = x, opt = opt))
