09f0118bf156a3ac21c9f02fbe626a829cf69959
Peter M C Harrison <pmc.harrison@gmail.com>
2019-02-07 12:57:02 +0000

    Improving profile-cost-funs

--- R/profile-cost-funs.R
+++ R/profile-cost-funs.R
@@ -1,12 +1,20 @@
 #' @export
-profile_cost_funs <- function(funs = voice_cost_funs()) {
-  x <- all_voicings_pc_set(c(0, 4, 7),
+profile_cost_funs <- function(random = TRUE, funs = voice_cost_funs()) {
+  if (random) {
+    a <- sort(sample(0:11, size = 3))
+    b <- sort(sample(0:11, size = 3))
+  } else {
+    a <- c(0, 4, 7)
+    b <- c(2, 7, 11)
+  }
+
+  x <- all_voicings_pc_set(a,
                            min_octave = - 2,
                            max_octave = 1,
                            dbl_change = TRUE,
                            dbl_min = 3,
                            dbl_max = 4)
-  y <- all_voicings_pc_set(c(2, 7, 11),
+  y <- all_voicings_pc_set(b,
                            min_octave = - 2,
                            max_octave = 1,
                            dbl_change = TRUE,
