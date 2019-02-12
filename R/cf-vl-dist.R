22367b87913d54647dba30073fe816d0bf3b126e
Peter M C Harrison <pmc.harrison@gmail.com>
2019-02-12 12:00:08 +0000

    Limiting the cache size for min_vls

--- R/cf-vl-dist.R
+++ R/cf-vl-dist.R
@@ -1,5 +1,14 @@
-.min_vls <- memoise::memoise(minVL::min_vls,
-                             cache = memoise::cache_memory())
+..min_vls_cache <- list(store = memoise::cache_memory(),
+                        counter = 0L) %>% as.environment()
+
+..min_vls <- memoise::memoise(minVL::min_vls, cache = ..min_vls_cache$store)
+
+.min_vls <- function(...) {
+  cache <- ..min_vls_cache
+  if (cache$counter > 1000L) memoise::forget(..min_vls)
+  cache$counter <- cache$counter + 1L
+  ..min_vls(...)
+}
 
 vl_dist <- (function(contexts, continuation) {
   stopifnot(is.list(contexts), is.numeric(continuation))
