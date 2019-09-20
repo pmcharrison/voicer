7e192edd2d4710b5c60f191da8d976bb237f16c2
Peter M C Harrison <pmc.harrison@gmail.com>
2019-09-20 11:18:54 +0100

    Updating to address new hrep alphabet representation

--- tests/testthat/test-pc_chord_type_ideal_voicings.R
+++ tests/testthat/test-pc_chord_type_ideal_voicings.R
@@ -1,12 +1,13 @@
 test_that("examples", {
-  i <- 100
-  pc_chord_type <- hrep::decode(hrep::coded_vec(i, "pc_chord_type"))[[1]]
+  pc_chord_type <- hrep::pc_chord_type(c(0, 1, 6, 9))
+  i <- as.integer(hrep::encode(pc_chord_type))
   voicing <- pc_chord_type_ideal_voicings[[i]]
   
   expect_equal(pc_chord_type, hrep::pc_chord_type(voicing))
-  expect_equal(pc_chord_type_ideal_voicings[[100]],
+  expect_equal(pc_chord_type_ideal_voicings[[i]],
                hrep::pi_chord(c(48, 54, 61, 66, 69)))
   
-  expect_equal(pc_chord_type_ideal_voicings[[1]],
+  j <- as.integer(hrep::encode(hrep::pc_chord_type(0)))
+  expect_equal(pc_chord_type_ideal_voicings[[j]],
                hrep::pi_chord(c(48, 60)))
 })
